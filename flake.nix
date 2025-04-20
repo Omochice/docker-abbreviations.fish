8417a82f4c97c9eafe09abb3e8f988acd3d5b2a9
Omochice <mochice.mls.ntl@gmail.com>
2025-04-20 17:59:04 +0900

    chore: add configurations

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "Docker abbreviations for fish";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      treefmt-nix,
+      ...
+    }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      treefmt =
+        system:
+        treefmt-nix.lib.evalModule nixpkgs.legacyPackages.${system} (
+          { ... }:
+          {
+            programs = {
+              # keep-sorted start block=yes
+              formatjson5 = {
+                enable = true;
+                indent = 2;
+              };
+              keep-sorted.enable = true;
+              mdformat.enable = true;
+              nixfmt.enable = true;
+              taplo.enable = true;
+              yamlfmt = {
+                enable = true;
+                settings = {
+                  formatter = {
+                    type = "basic";
+                    retain_line_breaks_single = true;
+                  };
+                };
+              };
+              # keep-sorted end
+            };
+          }
+        );
+    in
+    {
+      formatter = forAllSystems (system: (treefmt system).config.build.wrapper);
+    };
+}
